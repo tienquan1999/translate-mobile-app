6d8e57202ff3d9ff892a0535c342b5758623579c
diepninh <ninhhongdiep99@gmail.com>
2020-06-04 13:46:24 +0700

    12

--- src/components/HeaderDefinitionWord.js
+++ src/components/HeaderDefinitionWord.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {StyleSheet} from "react-native"
-import {Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import {StyleSheet,Text} from "react-native"
+import {Header, Left, Body, Right, Button, Icon, Title, View } from 'native-base';
 
 export default function HeaderDefinitionWord(props) {
   const {title, handleBack} = props;
   
   return (
-      <Header style={styles.header}>
+      <Header style={styles.headerTab}>
         <Left>
           <Button transparent onPress={handleBack}>
             <Icon name='arrow-back' />
           </Button>
         </Left>
         <Body>
-          <Title>{title}</Title>
+          <Text>{title}</Text>
         </Body>
         <Right>
           <Button transparent>
@@ -24,10 +24,11 @@ export default function HeaderDefinitionWord(props) {
           </Button>
         </Right>
       </Header>
+      
   );
 }
 const styles = StyleSheet.create({
-  header:{
+  headerTab:{
     backgroundColor: "#0077b3",
   }
 })
