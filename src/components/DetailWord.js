d55be32e42c2ab0b2a0f7091648748ed69cdc6f0
diepninh <ninhhongdiep99@gmail.com>
2020-06-09 19:35:27 +0700

    fix

--- src/components/DetailWord.js
+++ src/components/DetailWord.js
@@ -5,27 +5,33 @@ import {Header, Left, Body, Right, Button, Icon ,Text} from 'native-base';
 import {connect} from "react-redux"
 function DetailWord(props) {
   const {wordMeaning}=props;
-  console.log(wordMeaning);
+
   const dataWord = wordMeaning.data;
   let arrMean = dataWord.mean ;
-  let arrMeanNew = arrMean.splice((arrMean.length-1),1)
+   
   
   return (
 
     <View style={styles.body}>
       <ScrollView>
-        <Text style={styles.wordHeader}>{ dataWord.word}</Text>
+        <Text style={styles.wordHeader}>{dataWord.word }</Text>
         <View>
           <View style={styles.viewPronunciation}>
             <Image style={styles.image} source={require('../icon/listen.png')} />
-            <Text style={styles.pronunciation}>{dataWord.pronunciation }</Text>
+            <Text style={styles.pronunciation}>{dataWord.pronunciation  }</Text>
           </View>
          
         </View>
         <View>
           {arrMean.map(e => <View key={e.id}>
                 <Text style={styles.proper}>{e.type}</Text>
-                <Text style={styles.means}>{e.item1.txt}</Text>
+                 {e.values.map(en => <View>
+                   <Text style={styles.means}>{en.mean}</Text>
+                   {en.examples.map(em =><View>
+                     <Text style={styles.exmEng}>{em.word}</Text>
+                     <Text style={styles.exmVie}><Image style={styles.image2} source={require('../icon/circle-gray.png')}/>{em.mean}</Text>
+                   </View>)}
+                 </View>)}
                 
               </View>
               )}
@@ -40,7 +46,7 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: "#ffffff",
     paddingLeft: 20,
-    height: "100%"
+    height: "100%",
   },
   wordHeader: {
     fontSize: 40,
@@ -69,6 +75,22 @@ const styles = StyleSheet.create({
   },
   pronunciation : {
     paddingLeft : 20
+  },
+  exmEng : {
+    paddingLeft : 60,
+    fontStyle : "italic",
+    color : "#3E7FC1",
+    paddingTop : 10
+  },
+  exmVie:{
+    paddingLeft: 60,
+    color: "gray",
+    paddingTop : 10,
+  },
+  image2 :{
+    width : 10,
+    height: 10,
+     
   }
 })
 const mapStateToProps = (state) =>{
