926dac8ae658ea66937fdaa22b294f27e14c9ada
quanht <zed.quan99@gmail.com>
2020-06-12 02:38:18 +0700

    cache with table history

--- src/utils/controller.js
+++ src/utils/controller.js
@@ -5,19 +5,39 @@ const formatResult = require("./format-result-translate");
 
 async function translateText({from, to, word}){
     try{
+        let db = await connectToDatabase("translate.db");
         word = word.replace(/\s\s+/g, ' ');
         let result = {};
-        if(from === "en" && to === "vi" && word.split(" ").length <= 2){
-            result = await translateEnToVi(word);
-        } else if(from === "vi" && to === "en" && word.split(" ").length <= 2){
-            result = await translateViToEn(word);
-        }else{
-            result.length = 0;
+        let query = "select * from historyTranslate where word = ? and fromLanguage = ? and toLanguage = ?;";
+        result = await querySQLite({
+            db,
+            query,
+            params: [word, from, to]
+        })
+        result = JSON.parse(result);
+        if(result._array.length === 0 ){
+            if(from === "en" && to === "vi" && word.split(" ").length <= 2){
+                result = await translateEnToVi(word);
+            } else if(from === "vi" && to === "en" && word.split(" ").length <= 2){
+                result = await translateViToEn(word);
+            }else{
+                result.length = 0;
+            }
+            if(result.length === 0){
+                result = await translateWithGoogleApi({from, to, word});
+            }
+            result = formatResult(result);
+            query = "insert into historyTranslate(word, fromLanguage, toLanguage, result) values(?, ?, ?, ?);";
+            await querySQLite({
+                db,
+                query,
+                params: [word, from, to, JSON.stringify(result)]
+            })
+        }else {
+            console.log("Get data from cache");
+            result = JSON.parse(result._array[0].result);
         }
-        if(result.length === 0){
-            result = await translateWithGoogleApi({from, to, word});
-        }
-        return formatResult(result);
+        return result;
     }
     catch(e){
         throw e;
--- src/utils/database/index.js
+++ src/utils/database/index.js
@@ -1,11 +1,12 @@
 import * as SQLite from 'expo-sqlite';
 import * as FileSystem from 'expo-file-system';
 import {Asset} from "expo-asset";
-
+const {querySQLite} = require("./query");
 
 async function connectToDatabase(dbName) {
     const internalDbName = dbName; // Call whatever you want
     const sqlDir = FileSystem.documentDirectory + "SQLite/";
+    //await FileSystem.deleteAsync(sqlDir + internalDbName);
     let db = await FileSystem.getInfoAsync(sqlDir + internalDbName);
     if (!db.exists && (dbName === "enToVi.db" || dbName === "viToEn.db")) {
         console.log("load file ");
@@ -14,9 +15,16 @@ async function connectToDatabase(dbName) {
         const asset = Asset.fromModule(module);
         await FileSystem.downloadAsync(asset.uri, sqlDir + internalDbName);
     }else if(!db.exists){
+        console.log("init");
         let db = await SQLite.openDatabase(internalDbName);
         let query = "create table if not exists audio (id integer primary key AUTOINCREMENT, word text, path text);"
         await createTable({db, query})
+        query = "create table if not exists historyTranslate (id integer primary key AUTOINCREMENT, word text, fromLanguage text, toLanguage text, result text);"
+        await createTable({db, query});
+        query = "create index audioIndex on audio(word); create index historyIndex on historyTranslate(word);"
+        await querySQLite({
+            db, query, params: []
+        })
     }
     return await SQLite.openDatabase(internalDbName);
 }
